12d1f1aa28eec7eee785c72c83468818f0ff7562
Samuele Barbieri <samuele.barbieri95@gmail.com>
2019-10-18 13:58:06 +0200

    update launch.sh script to also manage virtualenv and dependencies

--- launch.sh
+++ launch.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/sh
 
+# Entering virtualenv
+test -f "venv/bin/activate"
+if [ $? == 1 ]; then
+    python3 -m venv venv
+fi
+. ./venv/bin/activate
+
+# Checking dependencies
+pip3 install -r requirements.txt --upgrade
+
+# Setting up database
 test -f "konta.db"
 if [ $? == 1 ]; then
     python3 setup.py
 fi
 
+# Launching app
 python3 app.py
